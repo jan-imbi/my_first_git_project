81dd294aba6e84da62f5d57ce917115ec0f0388f
Jan Meis <meis@imbi.uni-heidelberg.de>
2020-10-14 13:22:48 +0200

    pretty output

--- some_script_file.R
+++ some_script_file.R
@@ -1,8 +1,8 @@
 # Calculate surface area of earth
 
-print("If earth was a perfect sphere, its surface area would be:")
-cat(4 * pi * 6371^2, "km^2")
+cat("If earth was a perfect sphere, its surface area would be:\n", 
+formatC(4 * pi * 6371^2, big.mark = ",", format = "f", digits = 0), "km^2")
 
-print("Its volume would be:")
-cat(4/3 * pi * 6371^3, "km^3")
+cat("Its volume would be:\n",
+formatC(4/3 * pi * 6371^3, big.mark = ",", format = "f", digits = 0), "km^3")
 
