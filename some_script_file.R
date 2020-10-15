96245cebea2a7deb611bd41896fddd5ab0cc8fc4
Jan Meis <55784605+jan-imbi@users.noreply.github.com>
2020-10-15 10:04:22 +0200

    Update some_script_file.R

--- some_script_file.R
+++ some_script_file.R
@@ -1,4 +1,4 @@
 # Calculate surface area of earth
 
 cat("The earth's average distance from the sun is",
-    formatC(149160000, big.mark = ",", digits=0, format = "f"), "km.")
+    formatC(92684000, big.mark = ",", digits=0, format = "f"), "miles.")
